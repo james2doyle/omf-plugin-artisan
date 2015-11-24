c7142c3815099393c1bc40e063931eca8205cae6
cotne nazarashvili <cotne.95@gmail.com>
2015-11-24 20:37:45 +0400

    remove php file use awk instead closes #1

--- completions/artisan.fish
+++ completions/artisan.fish
@@ -1,5 +1,5 @@
 function __fish_artisan_complete
-	php artisan --no-ansi | sed "1,/Available commands/d" | php (dirname (status -f))/artisan_complete.php
+	php artisan --no-ansi | sed "1,/Available commands/d" | awk 'match($0, /^ +(\S+)  +(.+)/, a) {print a[1] "\t" a[2]}'
 end
 
 function __artisan_complete --on-variable PWD
--- completions/artisan_complete.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-while ($line = fgets(STDIN)) {
-	if (preg_match('/^ +([A-z0-9\-\:]+) +([A-z0-9\,\-\:\.\/\\ \(\)]+)/', $line, $matches)) {
-		echo "$matches[1]\t$matches[2]\n";
-	}
-}
